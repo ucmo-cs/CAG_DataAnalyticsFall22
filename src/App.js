8b399ad554d2319a14fe629692c5fdb615c535e6
amk53970 <amk53970@ucmo.edu>
2022-10-17 21:24:47 -0500

    add all project information

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+// Gotta have css!
 import './App.css';
+
+// component imports
+import Home from './components/pages/Home';
+import Deployment from './components/pages/Deployment';
+import Burndown from './components/pages/Burndown';
+import Burnup from './components/pages/Burnup';
+
+// lib imports
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div className = "App">
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/deployment" element={<Deployment />} />
+        <Route path="/burn-down" element={<Burndown />} />
+        <Route path="/burn-up" element={<Burnup />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,47 @@
+import { Button } from 'antd';
+import { useNavigate } from "react-router-dom";
+
+import styled from 'styled-components';
+
+function Menu() {
+
+    const navigate = useNavigate();
+    
+    const toDeploy = () => {
+        navigate('/deployment');
+    }
+
+    const toBurnDown = () => {
+        navigate('/burn-down');
+    }
+
+    const toBurnUp = () => {
+        navigate('/burn-up');
+    }
+
+    const backToHome = () => {
+        navigate('/');
+    }
+
+    return (
+        <>
+            <F1>
+                <Button type="primary" onClick={ backToHome } style={{width: 100}}>Back</Button>
+                <Button type="primary" onClick={ toDeploy } style={{width: 100}}>Deployment</Button>
+                <Button type="primary" onClick={ toBurnDown } style={{width: 100}}>Burn Down</Button>
+                <Button type="primary" onClick={ toBurnUp } style={{width: 100}}>Burn Up</Button>
+            </F1>
+        </>
+    )
+}
+
+export default Menu;
+
+const F1 = styled.div`
+    height: 70px;
+    width: 500px;
+    align-items: center;
+    display: flex;
+    padding: 8px 30px;
+    justify-content: space-between; 
+`
