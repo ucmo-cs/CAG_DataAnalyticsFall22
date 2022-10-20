f9e1259fd4504ee92efcd196bebbaf5055bb46ea
amk53970 <amk53970@ucmo.edu>
2022-10-20 12:28:01 -0500

    Changed it back

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -26,7 +26,7 @@ function Home() {
                 <Form name="basic" labelCol={{ span: 8 }} labelAlign="left"
                     wrapperCol={{ span: 16 }} initialValues={{ remember: true }} autoComplete="off">
                     <Form.Item
-                        label="Project name"
+                        label="Project name (Required)"
                         name="projectName"
                         rules={[{ required: true, message: "Please input a project name"}]}>  
                         <Input />
